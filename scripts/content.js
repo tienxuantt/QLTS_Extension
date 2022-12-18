399809b8e40d9e53c10d3afaec1407dbc034edbf
Tiến Xuân <tienxuantt@gmail.com>
2022-12-18 21:49:51 +0700

    Bổ sung try catch

--- scripts/content.js
+++ scripts/content.js
@@ -6,156 +6,168 @@ function addFixedAsset(obj){
 
 // Show form
 function showFormAdd(obj){
-    if($("#name-list").length > 0){
-        goToLink(true, 6, false, false);
-    }
+    goToLink(true, 6, false, false);
 }
 
 // Điều hướng tới trang thêm mới
 function goToLink(isTangMoi, _loaiHinh, _isCreateTSDA, _isTSQL) {
-		
-    if (isTangMoi === true) {
-        let _url = ""
-        if (_isTSQL == true) {
-            _url = "/TaiSan/Create?LoaiHinhTSId=" + _loaiHinh + "&loailydobiendongId=1&isTangMoi=true&isCreateTSDA=False&isTSQL=true";
-        }
-        else {
-            _url = "/TaiSan/Create?LoaiHinhTSId=" + _loaiHinh + "&loailydobiendongId=1&isTangMoi=true&isCreateTSDA=False&isTSQL=false";
-        }
+	try {
+        let _url = `/TaiSan/Create?LoaiHinhTSId=${_loaiHinh}&loailydobiendongId=1&isTangMoi=${isTangMoi}&isCreateTSDA=False&isTSQL=${_isTSQL}`;
+
         window.location.replace(_url);
-    }
-    else {
-        let _url = ""
-        if (_isTSQL == true) {
-            _url = "/TaiSan/Create?LoaiHinhTSId=" + _loaiHinh + "&loailydobiendongId=1&isTangMoi=false&isCreateTSDA=False&isTSQL=true";
-        }
-        else {
-            _url = "/TaiSan/Create?LoaiHinhTSId=" + _loaiHinh + "&loailydobiendongId=1&isTangMoi=false&isCreateTSDA=False&isTSQL=false";
-        }
-        window.location.replace(_url);
+    } catch (error) {
+        alert("Đã có lỗi xảy ra. Vui lòng nhấn F5");
     }
 }
 
 // Xử lý binding dữ liệu form
 function bindingDataForm(obj){
-    if(obj){
-        let columnConfig = obj.ColumnConfig,
-            data = obj.Data;
-
-        // Duyệt từng control để binding
-        if(columnConfig && columnConfig.length > 0){
-            columnConfig.filter(function(item){
-                switch(item.ControlType){
-                    case 1: // String
-                        bindingControlString(item, data);
-                        break;
-                    case 2: // Number
-                        bindingControlNumber(item, data);
-                        break;
-                    case 3: // DateTime
-                        bindingControlDateTime(item, data);
-                        break;
-                    case 4: // Combobox
-                        bindingControlCombobox(item, data);
-                        break;
-                    case 6: // Checkbox
-                        bindingControlCheckbox(item, data);
-                        break;
-                }
-            });
-
-            // Tự động bấm lưu
-            setTimeout(function(){
-                $(".card-header .btnAdd").click();
-
-                setTimeout(function(){
-                    if($(`[data-bb-handler="confirm"]`).is(":visible")){
-                        // Gửi thông báo đã xong
-                        pushNofitySuccess();
-                        $(`[data-bb-handler="confirm"]`).click();
+    try {
+        if(obj){
+            let columnConfig = obj.ColumnConfig,
+                data = obj.Data;
+    
+            // Duyệt từng control để binding
+            if(columnConfig && columnConfig.length > 0){
+                columnConfig.filter(function(item){
+                    switch(item.ControlType){
+                        case 1: // String
+                            bindingControlString(item, data);
+                            break;
+                        case 2: // Number
+                            bindingControlNumber(item, data);
+                            break;
+                        case 3: // DateTime
+                            bindingControlDateTime(item, data);
+                            break;
+                        case 4: // Combobox
+                            bindingControlCombobox(item, data);
+                            break;
+                        case 6: // Checkbox
+                            bindingControlCheckbox(item, data);
+                            break;
                     }
-                }, 400);
-            }, 600);
+                });
+    
+                // Tự động bấm lưu
+                setTimeout(function(){
+                    $(".card-header .btnAdd").click();
+    
+                    setTimeout(function(){
+                        if($(`[data-bb-handler="confirm"]`).is(":visible")){
+                            // Gửi thông báo đã xong
+                            pushNofitySuccess();
+                            $(`[data-bb-handler="confirm"]`).click();
+                        }
+                    }, 400);
+                }, 600);
+            }
         }
+    } catch (error) {
+        alert("Đã có lỗi xảy ra. Vui lòng nhấn F5");
     }
 }
 
 // Gửi lên để lấy tài sản tiếp theo
 function pushNofitySuccess(){
-    chrome.runtime.sendMessage("success", (response) => {});
+    try {
+        chrome.runtime.sendMessage("success", (response) => {});
+    } catch (error) {
+        alert("Đã có lỗi xảy ra. Vui lòng nhấn F5");
+    }
 }
 
 // Xử lý binding control String
 function bindingControlString(config, data){
-    let control = $(`#${config.Field_TSC}`);
+    try {
+        let control = $(`#${config.Field_TSC}`);
 
-    if(control.length > 0){
-        let value = data[config.Field_QLTS];
-
-        if(value){
-            control.val(value);
-            control.blur();
+        if(control.length > 0){
+            let value = data[config.Field_QLTS];
+    
+            if(value){
+                control.val(value);
+                control.blur();
+            }
         }
+    } catch (error) {
+        alert("Đã có lỗi xảy ra. Vui lòng nhấn F5");
     }
 }
 
 // Xử lý binding control Number
 function bindingControlNumber(config, data){
-    let control = $(`#${config.Field_TSC}`);
+    try {
+        let control = $(`#${config.Field_TSC}`);
 
-    if(control.length > 0){
-        let value = data[config.Field_QLTS];
+        if(control.length > 0){
+            let value = data[config.Field_QLTS];
 
-        if(value){
-            let valueFormat = value.toString().replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1.");
+            if(value){
+                let valueFormat = value.toString().replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1.");
 
-            control.val(value);
-            control.prev().val(valueFormat);
-            control.attr("aria-valuenow", value);
-            control.prev().attr("aria-valuenow", value);
+                control.val(value);
+                control.prev().val(valueFormat);
+                control.attr("aria-valuenow", value);
+                control.prev().attr("aria-valuenow", value);
+            }
         }
+    } catch (error) {
+        alert("Đã có lỗi xảy ra. Vui lòng nhấn F5");
     }
 }
 
 // Xử lý binding control Combobox
 function bindingControlCombobox(config, data){
-    let control = $(`ul#${config.Field_TSC}`);
+    try {
+        let control = $(`ul#${config.Field_TSC}`);
 
-    if(control.length > 0){
-        let value = data[config.Field_QLTS];
+        if(control.length > 0){
+            let value = data[config.Field_QLTS];
 
-        if(value){
-            control.find("li").each(function(){
-                let spanText = $(this).text();
+            if(value){
+                control.find("li").each(function(){
+                    let spanText = $(this).text();
 
-                if(spanText.toLocaleLowerCase().trim().includes(value.toLocaleLowerCase().trim())){
-                    $(this).click();
-                }
-            });
+                    if(spanText.toLocaleLowerCase().trim().includes(value.toLocaleLowerCase().trim())){
+                        $(this).click();
+                    }
+                });
+            }
         }
+    } catch (error) {
+        alert("Đã có lỗi xảy ra. Vui lòng nhấn F5");
     }
 }
 
 // Xử lý binding control DateTime
 function bindingControlDateTime(config, data){
-    let control = $(`#${config.Field_TSC}`);
+    try {
+        let control = $(`#${config.Field_TSC}`);
 
-    if(control.length > 0){
-        let value = data[config.Field_QLTS];
+        if(control.length > 0){
+            let value = data[config.Field_QLTS];
 
-        if(value){
-            control.val(value);
-            control.blur();
+            if(value){
+                control.val(value);
+                control.blur();
+            }
         }
+    } catch (error) {
+        alert("Đã có lỗi xảy ra. Vui lòng nhấn F5");
     }
 }
 
 // Xử lý binding control Checkbox
 function bindingControlCheckbox(config, data){
-    let controlId = data[config.Field_QLTS],
-        control = $(`#${controlId}`);
+    try {
+        let controlId = data[config.Field_QLTS],
+            control = $(`#${controlId}`);
 
-    if(control.length > 0 && !control.prop("checked")){
-       control.click();
+        if(control.length > 0 && !control.prop("checked")){
+            control.click();
+        }
+    } catch (error) {
+        alert("Đã có lỗi xảy ra. Vui lòng nhấn F5");
     }
 }
